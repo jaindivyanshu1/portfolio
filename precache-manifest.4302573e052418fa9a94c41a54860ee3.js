ff49243b0385cdcfa5bfc09ce45f85699be537f7
Divyanshu Jain <jaindivyanshu.personal@gmail.com>
2024-07-21 03:22:25 +0530

    Updates

---
+++ precache-manifest.4302573e052418fa9a94c41a54860ee3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021dba448cfa1f92fc60cd5d136d9589",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "eed9779ac94fdecfa970",
+    "url": "/portfolio/static/css/main.e4856fdc.chunk.css"
+  },
+  {
+    "revision": "4d3efb1d43dac7c7ef1b",
+    "url": "/portfolio/static/js/2.feec635f.chunk.js"
+  },
+  {
+    "revision": "eed9779ac94fdecfa970",
+    "url": "/portfolio/static/js/main.87bd46d5.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
